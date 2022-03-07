8d81d65f889a28ffb9648e0ba7f6921837139329
mskidwai <mskidwai@ncsu.edu>
2022-03-07 16:35:22 -0500

    work in progress

--- app/src/components/shared-textarea.js
+++ app/src/components/shared-textarea.js
@@ -2,24 +2,78 @@ import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import { useParams } from 'react-router-dom';
 
+
+
+
+
 function SharedTextarea(props) {
   const [value, setValue] = useState('');
   const [socket, setSocket] = useState();
+  //const [newVal, setNewVal] = useState('');
   const { room } = useParams();
   useEffect(() => {
     let host = window.location.hostname;
     if (host === "localhost") host += ":3001";
+    
     const s = io(host, { transports: ['websocket'], secure: true });
+    
+    //prompt the client for their name
+    // const name = prompt('What is your name?');
+    // appendMessage('You joined');
+    //we emit the name to new-user which the server is listening for
+    // s.emit('new-user', name)
+    //Send the name in the text area
+    // s.on('chat-message', data => {
+      // appendMessage(`${data.name}: ${data.message}`)
+    // })
+    //Send that the user has connected in the text area
+    // s.on('user-connected', name => {
+      // appendMessage(`${name} connected`)
+      // s.on('textarea', (v) => {
+        //Emits the current value to send to the newly joined socket
+        // s.emit(v, room);
+      // });
+    // })
+    //Send the user has disconnected in the text area
+    // s.on('user-disconnected', name => {
+      // appendMessage(`${name} disconnected`)
+    // })
+    
+
+
     s.emit('join-textarea', room);
-    s.on('textarea', (v) => {
-      setValue(v);
+
+    //Gets the value and sends it to the new state it received
+    s.on('userconnect', (justJoinedId) => {
+      // setValue(value);
+      s.emit('existingvalue', {giveValueToThisId: justJoinedId, value});
+      console.log('emitting existing value', value);
+      
+      //console.log('User has joined. obtained old value');
     });
 
+    
+    s.on('textarea', (v) => {
+      setValue(v);
+      //Emits the current value to send to the newly joined socket
+      //s.emit(v, room);
+    });
+    
+
     setSocket(s);
+
+    
+
+    // function appendMessage(message) {
+      // setValue(value + '\n' + message);
+      // s.emit('textarea', message)
+    // }
   }, []);
 
+  
+
   function onChangeHandler(e) {
-    setValue(e.target.value);
+    setValue(e.target.value); 
     socket.emit('textarea', e.target.value)
   }
 
--- server/index.js
+++ server/index.js
@@ -3,21 +3,80 @@ const PORT = 3001;
 const server = require('http').Server();
 const socket = require('socket.io');
 const io = socket(server, { transports: ['websocket'] });
+const users = {} // Stores the list of users
 const pty = require('node-pty');
 let terminalPtys = {}; // {roomName: ptyProcess}
 
-io.sockets.on('connection', socket => {
 
-    socket.on('join-textarea', (room) => {
+
+io.sockets.on('connection', socket => {
+    
+
+   
+
+    socket.on('join-textarea', async (room) => {
         socket.leaveAll();
         socket.join(room);
         console.log(socket.id, 'joining textarea room', room);
+        // emit a new event to let people know when someone joins
+        // When a new user joins we send a string with their name
+        // socket.on('new-user', name => {
+        //     users[socket.id] = name;
+        //     socket.broadcast.emit('user-connected', name)
+        //   })
+        //   //Send a chat message with their name
+        //   socket.on('send-chat-message', message => {
+        //     socket.broadcast.emit('chat-message', 
+        //     {message, name:     users[socket.id] } )
+        //   })
+        //   socket.on('disconnect', () => {
+        //     socket.broadcast.emit('user-disconnected', users[socket.id])
+        //     delete users[socket.id]
+        //   })
+
+        // socket.broadcast.emit("New user joined");
+        // socket.in(room).emit('New User joined');
+
+        // socket.on('userconnect', (newVal) => {
+        //     socket.in(room).emit('userconnect', newVal);
+        // });
+
+        // socket.on('userconnect', (newVal) => {
+
+
+        let justJoinedId = socket.id;
+        let roomsSockets = (await io.in(room).fetchSockets())[0].nsp.sockets;
+        let roomsSocketsIds = Array.from(roomsSockets.keys())
+            .filter(
+                function (e) {
+                    if (e !== justJoinedId) return true;
+                    else return false;
+                });
+
+        if(roomsSocketsIds > 1) {
+            roomsSockets.get(roomsSocketsIds[0]).emit('userconnect', justJoinedId);
+            console.log('filtered array of ids', roomsSocketsIds);
+        }
+        
+
+        // });
+
+        socket.on('existingvalue', ({giveValueToThisId, value}) => {
+            roomsSockets.get(giveValueToThisId).emit('textarea', value);
+            console.log('recieving existing value', value);
+        });
+
+
 
         socket.on('textarea', (value) => {
+            // when the existing socket gets this event emit a current value
+            // socket.in(room).emit('textarea', 'New User Joined');
             socket.in(room).emit('textarea', value);
+            // when the server recieves this value, emit a new event and send the value to the newly joined socket
         });
+        
     });
-
+ 
     socket.on('join-terminal', (room) => {
         if (!terminalPtys[room]) {
             const ptyProcess = pty.spawn(this.command, this.args || [], {
